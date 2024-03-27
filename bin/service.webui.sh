b09d6b7c0348f5563ee689d7d7125618a60ca08a
Boris Rybalkin <ribalkin@gmail.com>
2024-03-27 20:41:32 +0000

    fix args

--- bin/service.webui.sh
+++ bin/service.webui.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-exec ${DIR}/webui -driver ${DIR}/ytd-lp/ytd-lp -out /data/youtube -host /var/snap/youtube/current/webui.socket
+exec ${DIR}/webui \
+  -driver ${DIR}/ytd-lp/ytd-lp \
+  -out /data/youtube \
+  -host /var/snap/youtube/current/webui.socket \
+  -session /var/snap/youtube/current \
+  -db /var/snap/youtube/current/local.db
 
