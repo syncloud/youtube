775807aa29dcdb99ba0b8f9e817be769bbaa2650
Boris Rybalkin <ribalkin@gmail.com>
2024-03-28 17:10:55 +0000

    fix service

--- bin/service.webui.sh
+++ bin/service.webui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export PATH=$PATH:${DIR}/webui/bin
-exec ${DIR}/webui/bin/yt-dlp-webui \
+exec ${DIR}/webui/bin/python ${DIR}/webui/usr/bin/yt-dlp \
   -conf /var/snap/youtube/current/config/webui.yaml  \
   -db /var/snap/youtube/current/local.db
 
