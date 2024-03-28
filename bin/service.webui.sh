8f9ad98500bea2d8a3b483d91baad4934287c137
Boris Rybalkin <ribalkin@gmail.com>
2024-03-28 09:21:44 +0000

    yt-dlp, ffmpeg, no arm, config

--- bin/service.webui.sh
+++ bin/service.webui.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-exec ${DIR}/webui \
-  -driver ${DIR}/ytd-lp/ytd-lp \
-  -out /data/youtube \
-  -host /var/snap/youtube/current/webui.socket \
-  -session /var/snap/youtube/current \
+export PATH=$PATH:${DIR}/webui/bin
+exec ${DIR}/webui/bin/yt-dlp-webui \
+  -conf /var/snap/youtube/current/config/webui.yaml  \
   -db /var/snap/youtube/current/local.db
 
