75acfe989a8c36926053e8930402cdecd7df7428
Boris Rybalkin <ribalkin@gmail.com>
2024-03-28 17:09:09 +0000

    python

--- webui/test.sh
+++ webui/test.sh
@@ -4,4 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ${DIR}/../build/snap/webui/bin/yt-dlp-webui --help
 ${DIR}/../build/snap/webui/bin/ffmpeg --help
 ${DIR}/../build/snap/webui/bin/ffprobe --help
-${DIR}/../build/snap/webui/usr/bin/yt-dlp --help
+${DIR}/../build/snap/webui/bin/python --version
+${DIR}/../build/snap/webui/bin/python ${DIR}/../build/snap/webui/usr/bin/yt-dlp --help
