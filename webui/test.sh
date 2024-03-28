92e977a1898c1bb78d8d90fe664373cc7ce95676
Boris Rybalkin <ribalkin@gmail.com>
2024-03-28 17:00:38 +0000

    probe

--- webui/test.sh
+++ webui/test.sh
@@ -3,5 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ${DIR}/../build/snap/webui/bin/yt-dlp-webui --help
 ${DIR}/../build/snap/webui/bin/ffmpeg --help
-${DIR}/../build/snap/webui/bin/ffprob --help
+${DIR}/../build/snap/webui/bin/ffprobe --help
 ${DIR}/../build/snap/webui/usr/bin/yt-dlp --help
