7072cb48f6aab115441424dd5449a5727fdb16b9
Boris Rybalkin <ribalkin@gmail.com>
2024-03-30 22:27:21 +0000

    fix test

--- test/ui.py
+++ test/ui.py
@@ -40,9 +40,12 @@ def test_login(selenium, device_user, device_password):
 
 
 def test_download(selenium, device_user, device_password):
-    selenium.find_by(By.XPATH, "//svg[@data-testid='AddIcon']").click()
-    selenium.find_by(By.XPATH, "//div[@download='yt']")
-    #https://m.youtube.com/watch?v=x983nr0lXwo
+    selenium.find_by(By.XPATH, "//button[@aria-label='Home speed dial']").click()
+    selenium.find_by(By.XPATH, "//button[@aria-label='New download']").click()
+    selenium.find_by(By.XPATH, "////textarea").send_keys("https://m.youtube.com/watch?v=x983nr0lXwo")
+    selenium.find_by(By.XPATH, "//button[contains(.,'Start')]").click()
+    selenium.find_by(By.XPATH, "//div[contains(.,'Syncloud Introduction')]")
+    selenium.find_by(By.XPATH, "//span[contains(.,'Completed')]")
 
 
 def test_teardown(driver):
