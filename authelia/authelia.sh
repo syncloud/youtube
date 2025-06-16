b4a817efbe39daae424e7ae51ee016a13f00b5e7
Boris Rybalkin <ribalkin@gmail.com>
2025-06-16 14:11:57 +0100

    fix app

--- authelia/authelia.sh
+++ authelia/authelia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 DIR=$( cd "$( dirname "$0" )" && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
-${DIR}/lib/*-linux*/ld-*.so* --library-path ${LIBS} ${DIR}/app/authelia "$@"
+${DIR}/lib/*-linux*/ld-*.so* --library-path ${LIBS} ${DIR}/authelia "$@"
