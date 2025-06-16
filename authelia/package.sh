db27c4914d534824fbee2c7b60dcf3d3074abc38
Boris Rybalkin <ribalkin@gmail.com>
2025-06-16 13:59:22 +0100

    fix authelia 1

--- authelia/package.sh
+++ authelia/package.sh
@@ -5,5 +5,7 @@ cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/authelia
 mkdir -p ${BUILD_DIR}
 cp /app/authelia ${BUILD_DIR}
+cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
 cp ${DIR}/authelia.sh ${BUILD_DIR}
+ls -la ${BUILD_DIR}/
