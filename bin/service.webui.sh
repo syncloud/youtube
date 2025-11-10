993bf15479070b2f479aa1d77cef63827468d881
Boris Rybalkin <ribalkin@gmail.com>
2025-11-10 19:03:34 +0000

    fix webui restart

--- bin/service.webui.sh
+++ bin/service.webui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+/bin/rm -rf $SNAP_DATA/webui.socket
 export PATH=$PATH:${DIR}/webui/bin
 exec ${DIR}/webui/bin/yt-dlp-webui \
-  -conf /var/snap/youtube/current/config/webui.yaml  \
-  -db /var/snap/youtube/current/local.db
-
+  -conf $SNAP_DATA/config/webui.yaml \
+  -db $SNAP_DATA/local.db
